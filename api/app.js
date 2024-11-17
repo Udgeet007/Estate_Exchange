8c55b5ff55df746359834b1880188ea97dfb2676
Udgeet <bhatt8udgeet@gmail.com>
2024-11-17 21:30:11 +0530

    Protected Routes with JWT Cookie

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/testRoute.js";
 const app = express();
 
 const port = 8000;
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/test",testRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
