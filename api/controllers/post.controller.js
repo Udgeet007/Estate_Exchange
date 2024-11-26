8a4acf4ebadfc26634486dae0e9ddf98ce1ef46a
Udgeet <bhatt8udgeet@gmail.com>
2024-11-26 13:39:55 +0530

    Save Post function done.

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,5 +1,5 @@
 import prisma from "../lib/prisma.js";
-
+import jwt from 'jsonwebtoken';
 export const getPosts = async (req, res) => {
   const query = req.query;
   // console.log(query);
@@ -42,12 +42,57 @@ export const getPost = async (req, res) => {
         },
       }
     });
-    res.status(200).json(post);
+    const token = req.cookies?.token;
+
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+        if (!err) {
+          const saved = await prisma.savedPost.findUnique({
+            where: {
+              userId_postId: {
+                postId: id,
+                userId: payload.id,
+              },
+            },
+          });
+          res.status(200).json({ ...post, isSaved: saved ? true : false });
+        }
+      });
+    }
+    res.status(200).json({ ...post, isSaved: false });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get post" });
   }
 };
+    // let userId;
+    // const token = req.cookies?.token;
+    // if(!token){
+    //   userId = null;
+    // }else{
+    //   jwt.verify(token, process.env.JWT_SECRET_KEY, async(err, payload)=>{
+    //     if(err){
+    //       userId = null;
+    //     } else{
+    //       userId = payload.id;
+    //     }
+    //   });
+    // }
+
+//     const saved = await prisma.savedPost.findUnique({
+//       where:{
+//         userId_postId:{
+//           postId: id,
+//           userId,
+//         },
+//       },
+//     });
+//     res.status(200).json({...post, isSaved: saved ? true : false});
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({ message: "Failed to get post" });
+//   }
+// };
 
 export const addPost = async (req, res) => {
   const body = req.body;
--- client/src/pages/singlePage/singlePage.jsx
+++ client/src/pages/singlePage/singlePage.jsx
@@ -2,12 +2,33 @@ import "./singlePage.scss";
 import Slider from "../../components/slider/Slider";
 import Map from "../../components/map/Map";
 // import { singlePostData } from "../../lib/dummydata";
-import { useLoaderData } from "react-router-dom";
+import { useNavigate, useLoaderData } from "react-router-dom";
 import DOMPurify from "dompurify";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
 
 function SinglePage() {
   const post = useLoaderData();
-  console.log(post);
+  // console.log(post);
+  const [saved, setSaved] = useState(post.isSaved);
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  
+  const handleSave = async () => {
+    if (!currentUser) {
+      navigate("/login");
+    }
+    // AFTER REACT 19 UPDATE TO USEOPTIMISTIK HOOK
+    setSaved((prev) => !prev);
+    try {
+      await apiRequest.post("/users/save", { postId: post.id });
+    } catch (err) {
+      console.log(err);
+      setSaved((prev) => !prev);
+    }
+  };
+
   return (
     <div className="singlePage">
       <div className="details">
@@ -28,9 +49,12 @@ function SinglePage() {
                 <span>{post.user.username}</span>
               </div>
             </div>
-            <div className="bottom"   dangerouslySetInnerHTML={{
+            <div
+              className="bottom"
+              dangerouslySetInnerHTML={{
                 __html: DOMPurify.sanitize(post.postDetail.desc),
-              }}></div>
+              }}
+            ></div>
           </div>
         </div>
       </div>
@@ -121,9 +145,9 @@ function SinglePage() {
               <img src="/chat.png" alt="" />
               Send a Message
             </button>
-            <button>
+            <button onClick={handleSave} style={{backgroundColor: saved ? "#fece51" : "white"}}>
               <img src="/save.png" alt="" />
-              Save the Place
+              {saved ? "Place Saved" : "Save the Place"}
             </button>
           </div>
         </div>
