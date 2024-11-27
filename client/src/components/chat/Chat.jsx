3b9b6fbed7372f1e44fa8adb74671895c1792cbe
Udgeet <bhatt8udgeet@gmail.com>
2024-11-27 10:52:21 +0530

    implemented Socket.io real time chat

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./chat.scss";
 import { AuthContext } from "../../context/AuthContext";
 import apiRequest from "../../lib/apiRequest";
 import { format } from "timeago.js";
+import { SocketContext } from "../../context/SocketContext";
 
 export default function Chat({ chats }) {
   const [chat, setChat] = useState(null);
   const { currentUser } = useContext(AuthContext);
+  const { socket } = useContext(SocketContext);
 
   const handleOpenChat = async (id, receiver) => {
     try {
@@ -23,18 +25,49 @@ export default function Chat({ chats }) {
     const formData = new FormData(e.target);
     const text = formData.get("text");
 
-    if(!text) return;
-    try{
-      const res = await apiRequest.post("/messages/"+ chat.id, {text});
-      setChat(prev=>( {...prev, messages:[...prev.messages, res.data]}));
+    if (!text) return;
+    try {
+      const res = await apiRequest.post("/messages/" + chat.id, { text });
+      setChat((prev) => ({ ...prev, messages: [...prev.messages, res.data] }));
       e.target.reset();
-    }catch(err){
+      socket.emit("sendMessage", {
+        receiverId: chat.receiver.id,
+        data: res.data,
+      });
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
+
+  useEffect(() => {
+    const read = async () => {
+      try {
+        await apiRequest.put("/chats/read/" + chat.id);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (chat && socket) {
+      socket.on("getMessage", (data) => {
+        if (chat.id === data.chatId) {
+          setChat((prev) => ({ ...prev, messages: [...prev.messages, data] }));
+          read();
+        }
+      });
+    }
+    return () => {
+      socket.off("getMessage");
+    };
+  }, [socket, chat]);
   // console.log(chats);
+
+  // const testSocket = () =>{
+  //   socket.emit("text", "hi From Client");
+  // }
   return (
     <div className="chat">
+      {/* <button onClick={testSocket}></button> */}
       <div className="messages">
         <h1>Messages</h1>
         {chats.map((c) => (
@@ -42,7 +75,7 @@ export default function Chat({ chats }) {
             className="message"
             key={c.id}
             style={{
-              backgroundColor: c.seenBy.includes(currentUser.id)
+              backgroundColor: c.seenBy.includes(currentUser.id)||chat?.id === c.id
                 ? "white"
                 : "#fecd514e",
             }}
